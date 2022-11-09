eae3247a32256f97b2d12bc67404a93989cdb5f7
souza99 <jpfsouza99@gmail.com>
2022-11-09 20:45:39 -0300

    criando serviço QUERY

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-app.post('/events', (req, res) => {
-    const event = req.body;
-
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-
-    res.send({ status : 'ok' });
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
-    console.log('Listenig on 4005'); 
+  console.log("Listening on 4005");
 });
